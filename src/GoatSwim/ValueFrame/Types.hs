0410d019032c9ab73ddfc47dd7892d8f98b95e3f
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-04 16:49:35 +0100

    ValueFrame type definition

---
+++ src/GoatSwim/ValueFrame/Types.hs
@@ -0,0 +1,23 @@
+module GoatSwim.ValueFrame.Types
+( ValueFrame(..)
+) where
+
+import Data.Bits.Floating
+import Data.Word
+import GoatSwim.Util
+import qualified Data.ByteString as B
+
+-- | Succinct representation of a set of values.
+data ValueFrame = ValueFrame
+                  (Maybe Word32) -- ^ first value
+                  Int            -- ^ number of valid bits
+                  B.ByteString   -- ^ bits
+
+-- | Pretty-printing of the ValueFrame type.
+instance Show ValueFrame where
+  show (ValueFrame Nothing _ _)          = "ValueFrame EMPTY"
+  show (ValueFrame (Just x) len bits) =
+    unwords [ "ValueFrame"
+            , "frst=" ++ show (coerceToFloat x :: Float)
+            , take len $ map (bool '1' '0') (unpackBits bits) ]
+
