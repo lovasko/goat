0c5f8d886728e39dd9205384be9a25e7bbf66d06
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-04 16:49:01 +0100

    ValueFrame main exports

---
+++ src/GoatSwim/ValueFrame.hs
@@ -0,0 +1,10 @@
+module GoatSwim.ValueFrame
+( ValueFrame(..)
+, valueDecode
+, valueEncode
+) where
+
+import GoatSwim.ValueFrame.Decode
+import GoatSwim.ValueFrame.Encode
+import GoatSwim.ValueFrame.Types
+
