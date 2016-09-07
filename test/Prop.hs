394a276d285609479b846f6b17d3e9f44dfe5722
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-07 19:52:21 +0100

    renamed identity tests to contain id

--- test/Prop.hs
+++ test/Prop.hs
@@ -15,32 +15,32 @@ prop_alignTo :: [Bool]
 prop_alignTo xs = mod (length $ alignTo 8 False xs) 8 == 0
 
 -- | The two functions to/fromBools must form an identity when composed.
-prop_toFromBools :: [Bool]
-                 -> Property
-prop_toFromBools bools = length bools == 8 ==>
-                         bools == toBools (fromBools bools :: Word8)
+prop_idToFromBools :: [Bool]
+                   -> Property
+prop_idToFromBools bools = length bools == 8 ==>
+                           bools == toBools (fromBools bools :: Word8)
 
 -- | The two functions from/toBools must form an identity when composed.
-prop_fromToBools :: Word64
-                 -> Bool
-prop_fromToBools word = word == fromBools (toBools word)
+prop_idFromToBools :: Word64
+                   -> Bool
+prop_idFromToBools word = word == fromBools (toBools word)
 
 -- | The two functions un/packBits must form an identity when composed.
 -- The only difference is that the packBits function aligns the input
 -- to the upper-multiply of eight.
-prop_packUnpackBits :: [Bool]
+prop_idPackUnpackBits :: [Bool]
                     -> Bool
-prop_packUnpackBits xs = alignTo 8 False xs == unpackBits (packBits xs)
+prop_idPackUnpackBits xs = alignTo 8 False xs == unpackBits (packBits xs)
 
 -- | The two functions encode/decodeNumber must form an identity when
 -- composed. The test asserts the condition that at least one of the
 -- number's bits is valid.
-prop_encdecNumber :: Int
-                  -> Int64
-                  -> Property
-prop_encdecNumber len n = len >= 3 ==>
-                          inBounds lo hi n ==>
-                          n == decodeNumber len (encodeNumber len n)
+prop_idEncDecNumber :: Int
+                    -> Int64
+                    -> Property
+prop_idEncDecNumber len n = len >= 3 ==>
+                            inBounds lo hi n ==>
+                            n == decodeNumber len (encodeNumber len n)
   where
     lo = -2^(len-1)
     hi =  2^(len-1)-1
@@ -51,12 +51,12 @@ prop_encdecNumber len n = len >= 3 ==>
 -- empty list is explicitely handled in the pattern-matching clauses of
 -- the functions. A list starting with zero would get interpreted as a
 -- empty list by the Encode function and therefore is not allowed.
-prop_encdecTimeFrame :: [Word32]
-                     -> Property
-prop_encdecTimeFrame xs = let ys = nub $ sort xs in
-                          not (null xs) ==>
-                          head ys /= 0  ==>
-                          ys == timeDecode (timeEncode (reverse ys))
+prop_idEncDecTimeFrame :: [Word32]
+                       -> Property
+prop_idEncDecTimeFrame xs = let ys = nub $ sort xs in
+                            not (null xs) ==>
+                            head ys /= 0  ==>
+                            ys == timeDecode (timeEncode (reverse ys))
 
 -- | Print a name of the property test and execute the QuickCheck
 -- algorithm.
@@ -72,11 +72,11 @@ runTests :: IO [Result]
 runTests = mapM runTest tests
   where
     tests = [ ("alignTo        ", property prop_alignTo)
-            , ("toFromBools    ", property prop_toFromBools)
-            , ("fromToBools    ", property prop_fromToBools)
-            , ("packUnpackBits ", property prop_packUnpackBits)
-            , ("encdecNumber   ", property prop_encdecNumber)
-            , ("encdecTimeFrame", property prop_encdecTimeFrame)]
+            , ("toFromBools    ", property prop_idToFromBools)
+            , ("fromToBools    ", property prop_idFromToBools)
+            , ("packUnpackBits ", property prop_idPackUnpackBits)
+            , ("encdecNumber   ", property prop_idEncDecNumber)
+            , ("encdecTimeFrame", property prop_idEncDecTimeFrame)]
 
 -- | Evaluate test results and set appropriate process exit code.
 main :: IO ()
