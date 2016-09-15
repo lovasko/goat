fecf5cd8b8f673b626c55713a3c58fa798bf30f0
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-15 22:07:40 +0100

    ValueFrame implementation of Frame

--- src/GoatSwim/ValueFrame.hs
+++ src/GoatSwim/ValueFrame.hs
@@ -1,10 +1,20 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module GoatSwim.ValueFrame
 ( ValueFrame(..)
 , valueDecode
 , valueEncode
 ) where
 
+import Data.Bits.Floating
+
+import GoatSwim.Frame
 import GoatSwim.ValueFrame.Decode
 import GoatSwim.ValueFrame.Encode
 import GoatSwim.ValueFrame.Types
+
+instance Frame Float ValueFrame where
+  frameDecode = valueDecode
+  frameEncode = valueEncode
+  frameHead (ValueFrame first _ _) = fmap coerceToFloat first
 
