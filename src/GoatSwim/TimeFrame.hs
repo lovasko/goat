8afb4e0a48417954a155a9ad327697438b7db6dc
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-15 22:04:42 +0100

    TimeFrame implementation of Frame

--- src/GoatSwim/TimeFrame.hs
+++ src/GoatSwim/TimeFrame.hs
@@ -1,10 +1,20 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module GoatSwim.TimeFrame
 ( TimeFrame(..)
 , timeDecode
 , timeEncode
 ) where
 
+import Data.Word
+
+import GoatSwim.Frame
 import GoatSwim.TimeFrame.Decode
 import GoatSwim.TimeFrame.Encode
 import GoatSwim.TimeFrame.Types
+
+instance Frame Word32 TimeFrame where
+  frameDecode = timeDecode
+  frameEncode = timeEncode
+  frameHead (TimeFrame first _ _ _) = first
 
