f2d0a2235bf17889a5e2821292d201659aa67bf5
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-16 20:40:45 +0100

    moved orphaned instances closer to the class definition

--- src/GoatSwim/Frame.hs
+++ src/GoatSwim/Frame.hs
@@ -7,10 +7,26 @@ module GoatSwim.Frame
 , frameHead
 ) where
 
+import Data.Bits.Floating
+import Data.Word
+
+import GoatSwim.TimeFrame
+import GoatSwim.ValueFrame
+
 -- | Abstract compression frame that binds two types together. The type
 -- variable "r" stands for Raw, variable "c" for Compressed.
 class Frame r c where
   frameDecode :: c -> [r]     -- ^ decompress raw values
   frameEncode :: [r] -> c     -- ^ compress raw values
   frameHead   :: c -> Maybe r -- ^ first compressed raw value
+
+instance Frame Float ValueFrame where
+  frameDecode = valueDecode
+  frameEncode = valueEncode
+  frameHead (ValueFrame first _ _) = fmap coerceToFloat first
+
+instance Frame Word32 TimeFrame where
+  frameDecode = timeDecode
+  frameEncode = timeEncode
+  frameHead (TimeFrame first _ _ _) = first
 
