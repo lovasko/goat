1e91f191a6931ed5392e2c68fd1c5b7b457549e7
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-16 03:06:59 +0100

    added missing pattern matches

--- src/GoatSwim/Fluid.hs
+++ src/GoatSwim/Fluid.hs
@@ -38,7 +38,8 @@ fluidNew ls = Fluid ls [[]] []
 -- | Obtain the first element stored in the frame.
 fluidFirst :: Fluid r c
            -> Maybe r
-fluidFirst (Fluid _ (x:_)   _) = headMay x
+fluidFirst (Fluid _ []    _) = Nothing
+fluidFirst (Fluid _ (x:_) _) = headMay x
 
 -- | Obtain the first elements of each section blocks. Empty frames and
 -- lists are represented as Nothing.
@@ -65,10 +66,12 @@ fluidShift (Fluid ls@(l1, l2) rs cs) = Fluid ls newRs newCs
     newRs = []                    : bool rs (init rs) (length rs <= l1)
     newCs = frameEncode (last rs) : bool cs (init cs) (length cs <= l2)
 
--- | Add new value to the fluid.
+-- | Add new value to the fluid. Internally this function is _prepending_
+-- in front of all existing values.
 fluidAppend :: Fluid r c -- ^ old fluid
             -> r         -- ^ new value
             -> Fluid r c -- ^ new fluid
+fluidAppend (Fluid ls []     cs) val = Fluid ls [[val]]      cs
 fluidAppend (Fluid ls (r:rs) cs) val = Fluid ls ((val:r):rs) cs
 
 -- | Dump all stored values in the uncompressed form.
