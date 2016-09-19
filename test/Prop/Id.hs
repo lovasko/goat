b6d82bc03a9fdf5946f3d285b37ce34d781640c0
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-09-19 23:52:03 +0100

    identity testing for pack/unpackBits

--- test/Prop/Id.hs
+++ test/Prop/Id.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Prop.Id
 ( idProps
 ) where
@@ -6,9 +8,16 @@ import Data.List
 import Data.Word
 import Test.QuickCheck
 
+import qualified Data.ByteString as B
+
 import GoatSwim.TimeFrame
 import GoatSwim.Util
 import GoatSwim.ValueFrame
+
+
+instance Arbitrary B.ByteString where
+  arbitrary = fmap B.pack arbitrary
+  shrink xs = fmap B.pack (shrink (B.unpack xs))
 
 -- | All tests that assert the fact that encode/decode functions
 -- must form an identity when composed.
@@ -16,6 +25,8 @@ idProps :: [(String, Property)]
 idProps =
   [ ("idToFromBools",      property idToFromBools)
   , ("idFromToBools",      property idFromToBools)
+  , ("idPackUnpackBits",   property idPackUnpackBits)
+  , ("idUnpackPackBits",   property idUnpackPackBits)
   , ("idEncDecTimeFrame",  property idEncDecTimeFrame)
   , ("idEncDecValueFrame", property idEncDecValueFrame) ]
 
@@ -28,6 +39,16 @@ idFromToBools x = x == fromBools (toBools x)
 idToFromBools :: Property
 idToFromBools = forAll (vector 64) $ \xs ->
                 xs == toBools (fromBools xs :: Word64)
+
+-- | The two functions pack/unpackBits must form an identity when composed.
+idPackUnpackBits :: [Bool]
+                 -> Bool
+idPackUnpackBits xs = xs == take (length xs) (unpackBits (packBits xs))
+
+-- | The two functions unpack/packBits must form an identity when composed.
+idUnpackPackBits :: B.ByteString
+                 -> Bool
+idUnpackPackBits bs = bs == (packBits (unpackBits bs))
 
 -- | The two functions timeDecode/Encode must form an identity when
 -- composed.
