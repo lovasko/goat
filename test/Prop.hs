e6d27c88f13072db57e94727806727cc44c430e5
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-11-20 15:06:44 +0000

    overridable default test count

--- test/Prop.hs
+++ test/Prop.hs
@@ -1,3 +1,5 @@
+import Safe
+import System.Environment
 import System.Exit
 import Test.QuickCheck
 import Test.QuickCheck.Test
@@ -10,26 +12,31 @@ import GoatSwim.Util
 
 -- | Print a name of the property test and execute the QuickCheck
 -- algorithm.
-runTest :: (String, Property)
+runTest :: Args
+        -> (String, Property)
         -> IO Result 
-runTest (name, prop) = do
+runTest args (name, prop) = do
   result <- quickCheckWithResult args prop
   putStr $ unwords [name, output result]
   return result
-    where
-      args = stdArgs { maxSuccess=50000
-                     , maxDiscardRatio=10000
-                     , chatty=False }
 
 -- | Run all available property tests and collect results.
-runTests :: IO [Result]
-runTests = mapM runTest (idProps ++ uniqueProps ++ fluidProps)
+runTests :: Args
+         -> IO [Result]
+runTests args = mapM (runTest args) (idProps ++ uniqueProps ++ fluidProps)
+
+-- | Parse command-line options into test arguments. In case invalid or
+-- no arguments were provided, the test fallbacks into a default value.
+parseArguments :: [String]
+               -> Args
+parseArguments []    = stdArgs { maxSuccess=1000,           chatty=False }
+parseArguments (x:_) = stdArgs { maxSuccess=readDef 1000 x, chatty=False }
 
 -- | Evaluate test results and set appropriate process exit code.
 main :: IO ()
 main = do
-  putStrLn ""
-  putStrLn "Running property tests:"
-  results <- runTests
+  putStrLn "\nRunning property tests:"
+  args    <- getArgs
+  results <- runTests (parseArguments args)
   bool exitSuccess exitFailure (all isSuccess results)
 
