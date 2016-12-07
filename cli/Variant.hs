8237e9d65ce1fcb488421b6d8201afabebaf7429
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-07 06:33:51 +0000

    renamed variants

--- cli/Variant.hs
+++ cli/Variant.hs
@@ -8,15 +8,15 @@ module Variant
 import qualified Data.Text as T
 import qualified System.FilePath as F
 
--- | GoatSwim files come in two versions: compressed and normal.
-data Variant = VarCompressed -- ^ compressed content
-             | VarNormal     -- ^ uncompressed content
+-- | GoatSwim files come in two formats: raw and compressed.
+data Variant = VarZip -- ^ compressed content
+             | VarRaw -- ^ uncompressed content
 
 -- | Deduce the variant of a file based on its file extension.
 varIdentify :: FilePath              -- ^ file name
             -> Either T.Text Variant -- ^ variant
 varIdentify path
-  | ext == ".gs"  = Right VarNormal
-  | ext == ".gsc" = Right VarCompressed
+  | ext == ".gs"  = Right VarRaw
+  | ext == ".gsz" = Right VarZip
   | otherwise     = Left "unrecognized file extension"
   where ext = F.takeExtension path
