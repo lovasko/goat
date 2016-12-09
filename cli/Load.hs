3443e5af5b272840979c2a614c2a51d3679289ce
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-09 19:41:11 +0000

    renamed loading to raw/zip formats

--- cli/Load.hs
+++ cli/Load.hs
@@ -9,21 +9,21 @@ import qualified Data.Text as T
 
 import GoatSwim.TimeFrame
 import GoatSwim.ValueFrame
+import Format
 import Story
-import Variant
 import Util
 
 
 -- | Load a story from a file in the uncompressed format.
-loadNormal :: B.ByteString        -- ^ file content
-           -> Either T.Text Story -- ^ error | story
-loadNormal bs = eitherFirst T.pack (S.runGet rule bs)
+loadRaw :: B.ByteString        -- ^ file content
+        -> Either T.Text Story -- ^ error | story
+loadRaw bs = eitherFirst T.pack (S.runGet rule bs)
   where rule = many $ S.getTwoOf S.getWord32be S.getFloat32be
 
 -- | Load a story from a file in the compressed format.
-loadCompressed :: B.ByteString        -- ^ file content
-               -> Either T.Text Story -- ^ error | story
-loadCompressed bs = case S.runGet (S.getTwoOf S.get S.get) bs of
+loadZip :: B.ByteString        -- ^ file content
+        -> Either T.Text Story -- ^ error | story
+loadZip bs = case S.runGet (S.getTwoOf S.get S.get) bs of
   Left  err      -> Left  $ T.pack err
   Right (ts, vs) -> Right $ zip (timeDecode ts) (valueDecode vs)
 
@@ -32,7 +32,7 @@ storyLoad :: FilePath                 -- ^ file path
           -> IO (Either T.Text Story) -- ^ error | story
 storyLoad path = do
   content <- B.readFile path
-  return $ case varIdentify path of
-    Left  err           -> Left err
-    Right VarNormal     -> loadNormal content
-    Right VarCompressed -> loadCompressed content
+  return $ case fmtIdentify path of
+    Left  err    -> Left err
+    Right FmtRaw -> loadRaw content
+    Right FmtZip -> loadZip content
