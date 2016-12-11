e8cb8391ebe6486cc5d8df99f52acd6457a083c0
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-11 14:46:30 +0000

    time rendering with options

--- cli/Util.hs
+++ cli/Util.hs
@@ -16,7 +16,9 @@ eitherFirst f (Left a)  = Left (f a)
 eitherFirst _ (Right b) = Right b
 
 -- | Convert a timestamp to a human-readable form.
-showTime :: Word32 -- ^ time stamp
+showTime :: Bool   -- ^ keep in timestamp form
+         -> Word32 -- ^ timestamp
          -> T.Text -- ^ textual representation
-showTime ts = T.pack $ formatTime defaultTimeLocale "%F %T" ts'
-  where ts' = posixSecondsToUTCTime $ fromIntegral ts
+showTime True  ts = T.pack (show ts)
+showTime False ts = T.pack (formatTime defaultTimeLocale "%F %T" ts')
+  where ts' = posixSecondsToUTCTime (fromIntegral ts)
