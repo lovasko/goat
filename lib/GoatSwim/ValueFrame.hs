e939a20e919d20acea71987d49aac77d479702ab
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-24 03:40:20 +0100

    module docs for ValueFrame

--- lib/GoatSwim/ValueFrame.hs
+++ lib/GoatSwim/ValueFrame.hs
@@ -1,3 +1,14 @@
+{- |
+Module      : Codec.Goat.ValueFrame
+Description : Top-level header for value compression
+Copyright   : (c) Daniel Lovasko, 2016
+License     : BSD3
+
+Maintainer  : Daniel Lovasko <daniel.lovasko@gmail.com>
+Stability   : stable
+Portability : portable
+-}
+
 module GoatSwim.ValueFrame
 ( ValueFrame(..)
 , valueDecode
