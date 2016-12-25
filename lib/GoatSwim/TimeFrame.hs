27fd76c39531e4ead5395b455599fc388a1856d1
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-25 06:42:14 +0100

    removed the original library

--- lib/GoatSwim/TimeFrame.hs
+++
@@ -1,9 +0,0 @@
-module GoatSwim.TimeFrame
-( TimeFrame(..)
-, timeDecode
-, timeEncode
-) where
-
-import GoatSwim.TimeFrame.Decode
-import GoatSwim.TimeFrame.Encode
-import GoatSwim.TimeFrame.Types
