2c5d47c4a940a5a2f09e2801278c6343b388c2da
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-28 11:04:40 +0100

    improved docs

--- src/Codec/Goat.hs
+++ src/Codec/Goat.hs
@@ -8,7 +8,10 @@ Maintainer  : Daniel Lovasko <daniel.lovasko@gmail.com>
 Stability   : stable
 Portability : portable
 
-Goat is a time series compression algorithm implementation in pure Haskell.
+Goat is a time series compression implementation in pure Haskell. It is
+heavily based on Facebook's Gorilla algorithm [1].
+
+[1] http://www.vldb.org/pvldb/vol8/p1816-teller.pdf
 -}
 
 module Codec.Goat
