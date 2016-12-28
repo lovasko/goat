3062ec4da95413496c6c3664456dad6949416537
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-28 12:06:58 +0100

    improved docs

--- src/Codec/Goat/Frame.hs
+++ src/Codec/Goat/Frame.hs
@@ -1,6 +1,6 @@
 {- |
 Module      : Codec.Goat.Frame
-Description : Abstraction over different compressions
+Description : Abstraction over different types of compression frames
 Copyright   : (c) Daniel Lovasko, 2016
 License     : BSD3
 
@@ -9,7 +9,7 @@ Stability   : stable
 Portability : portable
 
 The Frame typeclass unites the interface to both TimeFrame and ValueFrame
-types. It is inteded only for internal use.
+types. Intended for internal use only.
 -}
 
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -30,15 +30,17 @@ import Codec.Goat.ValueFrame
 -- | Abstract compression frame that binds two types together. The type
 -- variable "r" stands for Raw, variable "c" for Compressed.
 class Frame r c where
-  frameDecode :: c -> [r]     -- ^ decompress raw values
-  frameEncode :: [r] -> c     -- ^ compress raw values
-  frameHead   :: c -> Maybe r -- ^ first compressed raw value
+  frameDecode :: c -> [r]     -- ^ decompress a frame into raw elements
+  frameEncode :: [r] -> c     -- ^ compress raw elements into a frame
+  frameHead   :: c -> Maybe r -- ^ first raw element of a frame
 
+-- | Binding of Float and ValueFrame.
 instance Frame Float ValueFrame where
   frameDecode = valueDecode
   frameEncode = valueEncode
   frameHead (ValueFrame first _ _) = fmap coerceToFloat first
 
+-- | Binding of Word32 and TimeFrame.
 instance Frame Word32 TimeFrame where
   frameDecode = timeDecode
   frameEncode = timeEncode
